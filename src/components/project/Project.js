16d0f4aa2795fb37f95eff1b016d1dc9cdcddae3
Flare <blastclever@gmail.com>
2022-07-03 18:59:18 +0200

    no message

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -3,15 +3,19 @@ import {
   Card,
   CardContent,
   CardMedia,
+  Collapse,
+  Paper,
+  Skeleton,
   Stack,
   Typography,
 } from "@mui/material";
 import { Box } from "@mui/system";
+import { useState } from "react";
 
-const Project = ({ picture, title, address }) => {
+export const ProjectSkeleton = () => {
   return (
     <Card sx={{ maxWidth: 800, height: 110, display: "flex" }}>
-      <CardMedia
+      {/* <CardMedia
         component="img"
         sx={{
           width: "100px",
@@ -21,6 +25,13 @@ const Project = ({ picture, title, address }) => {
         }}
         image={picture}
         alt="project's picture"
+      /> */}
+      <Skeleton
+        variant="rectangular"
+        sx={{
+          width: "100px",
+        }}
+        height="100%"
       />
       <CardContent
         sx={{
@@ -34,28 +45,90 @@ const Project = ({ picture, title, address }) => {
           justifyContent="space-between"
           sx={{ height: "100%", maxWidth: "max-content" }}
         >
-          <Typography
-            variant="body2"
-            color="primary"
-            sx={{ fontWeight: "bold" }}
-          >
-            {title}
-          </Typography>
-          <Typography variant="caption" color="primary">
-            {address}
-          </Typography>
+          <Skeleton variant="text" width={200} />
+          <Skeleton variant="text" width={100} />
         </Stack>
         <Stack
           direction="row-reverse"
           alignItems="end"
           sx={{ flex: 1, height: "100%" }}
         >
-          <Button variant="contained" sx={{ width: 100 }} color="error">
-            حذف
-          </Button>
+          <Skeleton variant="rectangular" width={100} height={36} />
         </Stack>
       </CardContent>
     </Card>
+  );
+};
+
+const Project = ({ picture, title, address, id }) => {
+  const [isDeleted, setIsDeleted] = useState(false);
+
+  const handleDelete = () => {
+    fetch("http://137.184.58.193:8000/aqar/api/router/Project/" + id + "/", {
+      method: "DELETE",
+      headers: {
+        //prettier-ignore
+        "Authorization": "Token 4b0d32e62fab4bf53d1907ab69cf6b3a9583eca1",
+      },
+    }).then(() => {
+      setIsDeleted(true);
+    });
+  };
+
+  return (
+    <Collapse in={!isDeleted}>
+      <Card sx={{ maxWidth: 800, height: 110, display: "flex" }}>
+        <CardMedia
+          component="img"
+          sx={{
+            width: "100px",
+            objectFit: "cover",
+            objectPosition: "center",
+            borderRadius: "4px",
+          }}
+          image={picture}
+          alt="project's picture"
+        />
+        <CardContent
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            width: "100%",
+            boxSizing: "border-box",
+          }}
+        >
+          <Stack
+            justifyContent="space-between"
+            sx={{ height: "100%", maxWidth: "max-content" }}
+          >
+            <Typography
+              variant="body2"
+              color="primary"
+              sx={{ fontWeight: "bold" }}
+            >
+              {title}
+            </Typography>
+            <Typography variant="caption" color="primary">
+              {address}
+            </Typography>
+          </Stack>
+          <Stack
+            direction="row-reverse"
+            alignItems="end"
+            sx={{ flex: 1, height: "100%" }}
+          >
+            <Button
+              variant="contained"
+              sx={{ width: 100 }}
+              color="error"
+              onClick={handleDelete}
+            >
+              حذف
+            </Button>
+          </Stack>
+        </CardContent>
+      </Card>
+    </Collapse>
   );
 };
 
--- src/pages/projects-display/ProjectsDisplay.js
+++ src/pages/projects-display/ProjectsDisplay.js
@@ -1,9 +1,42 @@
-import { Stack } from "@mui/material";
+import { Paper, Stack } from "@mui/material";
+import { useState } from "react";
+import { useEffect } from "react";
 import Parameter from "../../components/parameter/Parameter";
-import Project from "../../components/project/Project";
+import Project, { ProjectSkeleton } from "../../components/project/Project";
 import Wrapper from "../../components/wrapper/Wrapper";
 
+const ProjectSkeletonsStack = () => {
+  return (
+    <>
+      <ProjectSkeleton />
+      <ProjectSkeleton />
+      <ProjectSkeleton />
+      <ProjectSkeleton />
+      <ProjectSkeleton />
+      <ProjectSkeleton />
+      <ProjectSkeleton />
+    </>
+  );
+};
+
 const ProjectsDisplay = () => {
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    fetch("http://137.184.58.193:8000/aqar/api/router/Project/", {
+      method: "GET",
+      headers: {
+        //prettier-ignore
+        "Authorization": "Token 4b0d32e62fab4bf53d1907ab69cf6b3a9583eca1",
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        console.log(json);
+        setProjects(json);
+      });
+  }, []);
   return (
     <Wrapper>
       <Parameter
@@ -19,16 +52,29 @@ const ProjectsDisplay = () => {
         ]}
       />
       <Stack spacing={4}>
-        <Project
-          picture="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          title={"مشروع الشيخ زايد & أكتوبر"}
-          address={"اكتوبر غرب سوميد، مجاور 2، فيلا 10"}
-        />
-        <Project
-          picture="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          title={"مشروع بس مش عارف اسميه ايه"}
-          address={"بلا بلا بلا بلا"}
-        />
+        {projects.length <= 0 && <ProjectSkeletonsStack />}
+        {projects.length > 0 &&
+          // <>
+          //   <Project
+          //     picture="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          //     title={"مشروع الشيخ زايد & أكتوبر"}
+          //     address={"اكتوبر غرب سوميد، مجاور 2، فيلا 10"}
+          //   />
+          //   <Project
+          //     picture="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          //     title={"مشروع بس مش عارف اسميه ايه"}
+          //     address={"بلا بلا بلا بلا"}
+          //   />
+          // </>
+          projects.map((project, index) => (
+            <Project
+              key={index}
+              picture={project.logo}
+              title={project.name}
+              address={project.address}
+              id={project.id}
+            />
+          ))}
       </Stack>
     </Wrapper>
   );
